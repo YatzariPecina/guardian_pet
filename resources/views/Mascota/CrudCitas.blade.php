c4a37cd7c60fbd493c0c134b219e4079e4c0e8f7
2130074 <2130074@upv.edu.mx>
2024-10-08 07:37:03 -0600

    Banner

---
+++ resources/views/Mascota/CrudCitas.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Citas</title>
+</head>
+
+<body class="h-screen bg-white flex flex-col items-center">
+    <header class="bg-[#24CE6B] w-full flex justify-between items-center px-12 py-4">
+        <div class="flex items-center space-x-6">
+            <img src="img/logo_guardian_pet.png" alt="Guardian Pet Logo" class="w-8 h-8 object-contain">
+            <h1 class="text-xs font-bold text-black">Guardian Pet</h1>
+        </div>
+        <nav class="flex space-x-12">
+            <a href="#" class="text-black font-semibold hover:underline">Mascotas</a>
+            <a href="#" class="text-black font-semibold hover:underline">Recordatorios</a>
+            <a href="#" class="text-black font-semibold hover:underline">Veterinarios</a>
+        </nav>
+        <div>
+            <img src="img/user.png" alt="User Icon" class="w-6 h-6">
+        </div>
+    </header>    
+
+</body>
+
+</html>
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Inicio - Guardian Pet</title>
+    <title>Inicio</title>
 </head>
 
 <body class="h-screen bg-white flex flex-col items-center">
