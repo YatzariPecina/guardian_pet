790286bd6a5a17a93e2ef2d3810e736d2910c8ac
2130074 <2130074@upv.edu.mx>
2024-10-08 07:32:17 -0600

    Pantallas creadas con el de navegacion

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,43 @@ Route::get('/registro', function () {
 Route::get('/inicio', function () {
     return view('inicio');
 });
+
+Route::get('/Carnet', function () {
+    return view('Mascota.Carnet');
+});
+
+Route::get('/CrudCitas', function () {
+    return view('Mascota.CrudCitas');
+});
+
+Route::get('/CrudMascota', function () {
+    return view('Mascota.CrudMascota');
+});
+
+Route::get('/EditarDatos', function () {
+    return view('Mascota.EditarDatos');
+});
+
+Route::get('/RegistroCita', function () {
+    return view('Mascota.RegistroCita');
+});
+
+Route::get('/RegistroMascota', function () {
+    return view('Mascota.RegistroMascota');
+});
+
+Route::get('/CrudVeterinarios', function () {
+    return view('Veterinario.CrudVeterinarios');
+});
+
+Route::get('/DetallesVeterinario', function () {
+    return view('Veterinario.DetallesVeterinario');
+});
+
+Route::get('/MisVeterinarios', function () {
+    return view('Veterinario.MisVeterinarios');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
