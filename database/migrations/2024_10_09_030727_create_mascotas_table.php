6ea19f5b3f799b2cce2ae79433e4bb3067655ab9
DamarisEspinosa <2130081@upv.edu.mx>
2024-10-08 21:41:43 -0600

    migraciones funcionando

---
+++ database/migrations/2024_10_09_030727_create_mascotas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 30);
+            $table->string('especie', 30);
+            $table->string('raza', 30);
+            $table->integer('edad');
+            $table->string('sexo', 10);
+            $table->string('caracteristicas');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mascotas');
+    }
+};
---
+++ database/migrations/2024_10_09_031800_create_veterinarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('veterinarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('dirección');
+            $table->string('numero_telefono', 10);
+            $table->string('correo', 50);
+            $table->string('especialidad', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('veterinarios');
+    }
+};
